db79684f56399a0b9ecda809cde6f76644445177
seasontop <xiechaoning@gmail.com>
2011-09-23 18:55:51 +0800

    add uint test update Query.js

--- Query.js
+++ Query.js
@@ -6,6 +6,9 @@ var Query = (function() {
     Query.fn = Query.prototype = {
         constructor: Query,
         init: function(select) {
+            if(Query.isFunction(select)) {
+                return 
+            }
             try {
                 var ele = document.querySelectorAll(select);
                 this.length = ele.length;
@@ -13,6 +16,9 @@ var Query = (function() {
                 ele = [];
             }
             return this.merge(this, ele);
+        },
+        ready: function() {
+            
         },
         merge: function(s, t) {
             var i = 0, tlen = t.length;
@@ -30,14 +36,82 @@ var Query = (function() {
     Query.extend = Query.fn.extend = function() {
         var arg0 = arguments[0];
         for(var i in arg0) {
-            var isExist = Query.fn[i];
+            var isExist = this[i];
             if(isExist) { continue; }
             var o = arg0[i];
-            Query.fn[i] = o;
+            this[i] = o;
         }
     };
 return Query;
 })();
+
+var class2type = {
+	    "[object Boolean]":"boolean",
+	    "[object Number]":"number",
+	    "[object String]":"string",
+	    "[object Function]":"function",
+	    "[object Array]":"array",
+	    "[object Date]":"date",
+	    "[object RegExp]":"regexp",
+	    "[object Object]":"object"
+	},
+    toString = Object.prototype.toString;
+
+Query.extend({
+    parseJSON: function(str) {
+	    return (new Function("return " + str))();
+	},
+    trim: function(str) {
+	    return str == null ? '' : str.toString().replace(/^[\s\xA0]+/, '').replace(/[\s\xA0]+$/, '');
+	},
+    isFunction: function(fn) {
+	    return Query.type(fn) === 'function';
+	},
+    type: function(obj) {
+	    return obj == null ? String( obj ) : class2type[ toString.call(obj) ] || "object";
+	},
+    escapeHTML: function(str) {
+	    var div = document.createElement('div'),
+	            text = document.createTextNode(str);
+	        div.appendChild(text);
+	        return div.innerHTML;
+	},
+    merge: function(source, target) {
+	    var len = arguments.length;
+	    if(len < 1) { return; }
+	    if(len < 2) { return arguments[0]; }
+	    var r = {};
+	    for(var i in source) {
+	        r[i] = typeof target[i] == 'undefined' ? source[i] : target[i];
+	    }
+	    return r;
+	},
+    /**
+     * 异步加载脚本
+     * @param pros {String||Object} 如果只有一个属性那么为src, 如果为多属性，请使用JSON格式 
+     * @param callback {Function} 回调函数
+     */
+    getScript: function() {
+	    var script = document.createElement('script'),
+            pros = arguments[0],
+            callback = arguments[1];
+        script.onload = script.onreadystatechange = function() {
+            if (!this.readyState || this.readyState === "loaded"
+                    || this.readyState === "complete") {
+                callback && callback();
+                script.onload = script.onreadystatechange = null;
+            }
+        };
+        if(Query.type(pros) === 'string') {
+            script.src = pros;
+        }else {
+            for(var name in pros) {
+                script.setAttribute(name, pros[name]);
+            }
+        }
+        document.getElementsByTagName('head')[0].appendChild(script);
+	}
+});
 window.$ = window.Q = Query;
 })(window);
 
---
+++ test/queryUnitTest.js
@@ -0,0 +1,55 @@
+//定义测试模块
+module( "Query" );
+//定义一个简单的函数，判断参数是不是数字
+//开始单元测试
+test('$.trim()', function() {
+    equal($.trim(' 123123'), '123123', '移除字符第一个空格');
+    equal($.trim('123123 '), '123123', '移除字符最后一个空格');
+    equal($.trim(' 123123 '), '123123', '移除字符首尾空格');
+});
+
+test('$.parseJSON()', function() {
+    deepEqual($.parseJSON('{ a: 1 }'), { a: 1 }, '两对像相等');
+    notDeepEqual($.parseJSON('{ a: 2 }'), { a: 1 }, '两对像不相等');
+});
+
+test('$.isFunction()', function() {
+    ok($.isFunction(function() {}), '是函数');
+    ok(!$.isFunction('12'), '不是函数');
+});
+
+test('$.type()', function() {
+    equal($.type(function(){}), 'function', '是函数');
+    equal($.type(true), 'boolean', '是boolean类型');
+    equal($.type('aa'), 'string', '是string类型');
+    equal($.type(12), 'number', '是number类型');
+    equal($.type([]), 'array', '是array类型');
+    equal($.type(new Date()), 'date', '是date类型');
+    equal($.type(/12/), 'regexp', '是regExp类型');
+    equal($.type({}), 'object', '是object类型');
+});
+
+test('$.escapeHTML()', function() {
+    equal($.escapeHTML('<script>alert("12");</script>'), "&lt;script&gt;alert(\"12\");&lt;/script&gt;", 'ok');
+});
+
+test('$.merge()', function() {
+    deepEqual($.merge(), undefined, '没有参数直接返回');
+    deepEqual($.merge({ a: 1 }), { a: 1 }, '只有一个参数，则返回此参数值');
+    deepEqual($.merge({}, { a: 1 }), {}, '空与B并为空');
+    deepEqual($.merge({ a: 1 }, {}), { a: 1 }, 'A与空合并为A');
+    deepEqual($.merge({ a: 1 }, { b: 1 }), { a: 1 }, 'A与B无交集合并后为A');
+    deepEqual($.merge({ c: 2, a: 1 }, { b: 1, a: 2 }), { c: 2, a: 2 }, 'A与B有交集合并后为A所有值保存，更新与B有交集的值。');
+});
+
+test('脚本加载测试', function() {
+    stop();
+    $.getScript('source/loadTest.js', function() {
+         $.getScript({
+            id: 'xiecn',
+            src: 'source/loadTest.js'
+         }, function() {
+	         start();       
+	    });      
+    });
+});
