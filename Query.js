d153489317a28f8dd31da943c65d7d6ed8405667
seasontop <xiechaoning@gmail.com>
2011-09-23 14:01:00 +0800

    update extend mathod

--- Query.js
+++ Query.js
@@ -9,14 +9,18 @@ var Query = (function() {
     Query.fn = Query.prototype = {
         constructor: Query,
         init: function(select) {
-            var ele = document.querySelectorAll(select);
-            this.length = ele.length;
+            try {
+                var ele = document.querySelectorAll(select);
+                this.length = ele.length;
+            }catch(e) {
+                ele = [];
+            }
             return this.merge(this, ele);
         },
         merge: function(s, t) {
             var i = 0, tlen = t.length,
                 slen = s.length;
-            slen = !slen ? 0 : slen;
+            slen = !slen ? 0 : slen - 1;
             for(i; i < tlen; i++) {
                 s[slen + i] = t[i];
             }
@@ -31,8 +35,8 @@ var Query = (function() {
     Query.extend = Query.fn.extend = function() {
         var arg0 = arguments[0];
         for(var i in arg0) {
-            var o = arg0[0];
-            this[i] = o;
+            var o = arg0[i];
+            Query.fn[i] = o;
         }
     };
 return Query;
